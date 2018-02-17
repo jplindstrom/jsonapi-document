080f5b9dd852540759194c51364c5be23eb2a2d5
Ali <ziali088@gmail.com>
2018-02-17 01:46:17 +0000

    perltidy

--- lib/JSONAPI/Document.pm
+++ lib/JSONAPI/Document.pm
@@ -5,45 +5,52 @@ package JSONAPI::Document;
 use Moo;
 
 use Lingua::EN::Inflexion ();
-use Carp ();
+use Carp                  ();
 
 sub compound_resource_document {
-    my ($self, $row, $options) = @_;
+    my ( $self, $row, $options ) = @_;
 
-    my $document = $self->resource_document($row, { with_relationships => 1 });
+    my $document =
+      $self->resource_document( $row, { with_relationships => 1 } );
+
+    my @includes;
+    my @relationships =
+      @{ $options->{includes} // [] } || $row->result_source->relationships();
+    foreach my $relation (@relationships) {
+        my $result = $self->_related_resource_documents( $row, $relation, { with_attributes => 1 } );
+        if ($result) {
+            push @includes, @$result;
+        }
+    }
 
     return {
-        data => [ $document ],
-        included => [
-            map {
-                @{ $self->_related_resource_documents($row, $_, { with_attributes => 1 }) }
-            } @{$options->{includes} // []} || $row->result_source->relationships(),
-        ]
+        data     => [$document],
+        included => \@includes,
     };
 }
 
 sub resource_documents {
-    my ($self, $resultset, $options) = @_;
+    my ( $self, $resultset, $options ) = @_;
     $options //= {};
 
     my @results = $resultset->all();
     return {
-        data => [ map { $self->resource_document($_, $options) } @results ],
+        data => [ map { $self->resource_document( $_, $options ) } @results ],
     };
 }
 
 sub resource_document {
-    my ($self, $row, $options) = @_;
+    my ( $self, $row, $options ) = @_;
     Carp::confess('No row provided or not a DBIx::Class:Row instance')
-        unless $row && $row->isa('DBIx::Class::Row');
+      unless $row && $row->isa('DBIx::Class::Row');
 
     $options //= {};
 
-    my $type = lc($row->result_source->source_name());
+    my $type = lc( $row->result_source->source_name() );
     my $noun = Lingua::EN::Inflexion::noun($type);
 
     my %columns = $row->get_inflated_columns();
-    my $id = delete $columns{id} // $row->id;
+    my $id      = delete $columns{id} // $row->id;
 
     unless ( $type && $id ) {
         # Document is not valid without a type and id.
@@ -52,10 +59,10 @@ sub resource_document {
 
     my %relationships;
     if ( $options->{with_relationships} ) {
-        my @relations = @{$options->{relationships} // []} || $row->result_source->relationships();
-        foreach my $rel ( @relations ) {
+        my @relations = @{ $options->{relationships} // [] } || $row->result_source->relationships();
+        foreach my $rel (@relations) {
             if ( $row->has_relationship($rel) ) {
-                my $docs = $self->_related_resource_documents($row, $rel);
+                my $docs = $self->_related_resource_documents( $row, $rel );
                 $docs = $docs->[0] if ( scalar(@$docs) == 1 );
                 $relationships{$rel} = { data => $docs };
             }
@@ -64,8 +71,8 @@ sub resource_document {
 
     my %document;
 
-    $document{id} = $id;
-    $document{type} = $noun->plural;
+    $document{id}         = $id;
+    $document{type}       = $noun->plural;
     $document{attributes} = \%columns;
 
     if ( values(%relationships) ) {
@@ -76,7 +83,7 @@ sub resource_document {
 }
 
 sub _related_resource_documents {
-    my ($self, $row, $relation, $options) = @_;
+    my ( $self, $row, $relation, $options ) = @_;
     $options //= {};
 
     my @results;
@@ -84,28 +91,30 @@ sub _related_resource_documents {
     my $rel_info = $row->result_source->relationship_info($relation);
     if ( $rel_info->{attrs}->{accessor} eq 'multi' ) {
         my @rs = $row->$relation->all();
-        foreach my $rel_row ( @rs ) {
+        foreach my $rel_row (@rs) {
             my %attributes;
             if ( $options->{with_attributes} ) {
                 %attributes = $rel_row->get_inflated_columns();
             }
 
-            push @results, {
-                id => delete $attributes{id} // $rel_row->id,
-                type => Lingua::EN::Inflexion::noun(lc($rel_row->result_source->source_name()))->plural,
+            push @results,
+              {
+                id   => delete $attributes{id} // $rel_row->id,
+                type => Lingua::EN::Inflexion::noun( lc( $rel_row->result_source->source_name()) )->plural,
                 values(%attributes) ? ( attributes => \%attributes ) : (),
-            };
+              };
         }
     }
     else {
         my %attributes = $row->$relation->get_inflated_columns();
-        my $id = delete $attributes{id} // $row->$relation->id;
+        my $id         = delete $attributes{id} // $row->$relation->id;
 
-        push @results, {
-            id => $id,
-            type => Lingua::EN::Inflexion::noun(lc($row->$relation->result_source->source_name()))->plural,
+        push @results,
+          {
+            id   => $id,
+            type => Lingua::EN::Inflexion::noun( lc( $row->$relation->result_source->source_name()) )->plural,
             $options->{with_attributes} ? ( attributes => \%attributes ) : (),
-        };
+          };
     }
 
     return \@results;
